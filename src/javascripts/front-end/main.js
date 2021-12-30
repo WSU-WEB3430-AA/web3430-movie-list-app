4bdc3fc3942152bb185cac53a8cd952ecf6f6e04
Abdulmalek Al-Gahmi <aalgahmi@gmail.com>
2021-12-30 10:56:25 -0700

    Update main.js

--- src/javascripts/front-end/main.js
+++ src/javascripts/front-end/main.js
@@ -3,7 +3,6 @@ require('../../favicon.ico')
 require.context('../../fonts/', true, /\.(eot|ttf|woff|woff2)$/i)
 require.context('../../images/', true, /\.(png|jpg|jpeg|gif|svg)$/i)
 require.context('../../stylesheets/', true, /\.(css|scss)$/i)
-require.context('../../', true, /\.(json|txt|dat)$/i)
 
 // TODO
 import 'bootstrap'
